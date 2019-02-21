734808080763bc4e74c0552a3335669e096ddf80
VanderDev1 <thomasv@vandervision.com>
2019-02-21 12:26:59 -0800

    uploaded project

---
+++ PhotoMatic/PhotoMatic/model/PhotoFetchService.swift
@@ -0,0 +1,38 @@
+//
+//  PhotoFetchService.swift
+//  PhotoMatic
+//
+//  Created by Thomas Vandegriff on 2/20/19.
+//  Copyright © 2019 Make School. All rights reserved.
+//
+
+import Foundation
+
+/*** PhotoFetchService.swift is an API Service client that is designed to:
+    - Fetch, post and process data to and from the target web services
+    - Serialize JSON data for manipulation and presentation
+    - Provide constructs for handling the successful or failed state of web service requests and responses
+ ***/
+
+
+
+  //TODO: Place Enums for Network calls and JSON processing functions here...
+
+
+struct PhotoFetchService {
+    
+    //TODO: Put your API Key, baseURLString, and flickrMethod method variables here...
+    
+ 
+    
+    //TODO: Place any variables required for Network calls and JSON processing functions here...
+
+
+   
+    //TODO: Refactor/move Network calls and JSON processing functions here...
+    
+
+    
+}
+
+
